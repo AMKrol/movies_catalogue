ab5f153e1094b01eecf62152ebca9413d08699be
Adrian Krol <adriankrol11@gmail.com>
2021-07-08 15:21:13 +0200

    active list indicator

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def homepage():
     list_type = request.args.get('list_type', "popular")
     movies = tmdb_client.get_movies(how_many=8, list_name=list_type)
-    list_types = ['popular', 'top_rated', 'upcoming', 'popular']
+    list_types = ['popular', 'top_rated', 'upcoming', 'now_playing']
     return render_template("homepage.html", movies=movies,
                            list_type=list_type, list_types=list_types)
 
