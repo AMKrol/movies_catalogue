d1c7abb61e02143d8da47fca3235a0841038e6bb
AdrianKrol <81975648+Thagord@users.noreply.github.com>
2021-07-10 15:23:46 +0200

    Tmdb client as class (#1)
    * objective programming
    
    * delete test.py
    
    * update endpoints
    
    * update main.py

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 from flask import Flask, render_template, request
-import tmdb_client
+from endpoints import TmdbService
 
 app = Flask(__name__)
+
+tmdb_client = TmdbService()
 
 
 @app.route('/')
 def homepage():
     list_type = request.args.get('list_type', "popular")
-    list_types = ['popular', 'top_rated', 'upcoming', 'now_playing']
-
-    if list_type not in list_types:
-        list_type = "popular"
-
-    movies = tmdb_client.get_movies(how_many=8, list_name=list_type)
-
-    return render_template("homepage.html", movies=movies,
-                           list_type=list_type, list_types=list_types)
+    movies = tmdb_client.get_movies_endpoint(how_many=8, list_name=list_type)
+    return render_template("homepage.html",
+                           movies=movies,
+                           list_type=list_type,
+                           list_types=tmdb_client.list_types)
 
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
-        return tmdb_client.get_poster_url(path, size)
+        return tmdb_client.get_poster_url_endpoint(path, size)
     return {"tmdb_image_url": tmdb_image_url}
 
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
-    details = tmdb_client.get_single_movie(movie_id)
-    cast = tmdb_client.get_cast(movie_id, how_many=16)
-    backdrop = tmdb_client.get_random_backdrop(movie_id)
-    return render_template("movie_details.html", movie=details,
-                           cast=cast, backdrop=backdrop)
+    return render_template("movie_details.html",
+                           movie=tmdb_client.get_single_movie_endpoint(
+                               movie_id),
+                           cast=tmdb_client.get_cast_endpoint(
+                               movie_id, how_many=16),
+                           backdrop=tmdb_client.get_random_backdrop_endpoint(movie_id))
 
 
 if __name__ == '__main__':
